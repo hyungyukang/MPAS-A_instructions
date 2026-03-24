310237615108b582a6709a567387e93a876b89d4
Hyun Kang <kangh@ornl.gov>
2026-03-24 00:05:26 -0400

    Update doc frame

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,10 +19,28 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "myst_parser",
+#    "sphinx_rtd_theme",
+#    "sphinx.ext.intersphinx",
+#    "sphinx.ext.viewcode",
+#    "sphinx.ext.mathjax",
+]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+root_doc = "index"
+
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
---
+++ docs/source/index.md
@@ -0,0 +1,8 @@
+(mpasa-home)=
+# MPAS-A instruction
+
+MPAS-A instructions documentation
+
+```{toctree}
+   :maxdepth: 1
+   :caption: Contents:
